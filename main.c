8b4359b49f23b16c7099faefd2e3558978c23c5e
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-08-07 23:19:24 +0000

    typedef types to unambiguous names

--- main.c
+++ main.c
@@ -6,15 +6,17 @@
 #define CRP_ERR 0
 #define CRP_OK 1
 
-typedef unsigned char byte;
+typedef unsigned char u8;
+typedef unsigned int u32;
+typedef int i32;
 
-void hexdump(byte *in, unsigned int len) {
-    for (unsigned int i = 0; i < len; ++i)
+void hexdump(u8 *in, u32 len) {
+    for (u32 i = 0; i < len; ++i)
         printf("%hhx", in[i]);
     printf("\n");
 }
 
-int rand_bytes(byte *out, unsigned int size) {
+i32 rand_bytes(u8 *out, u32 size) {
     FILE *urand = fopen("/dev/urandom", "r");
     if (!urand) {
         printf("cannot open '/dev/urandom/'. %s\n", strerror(errno));
@@ -22,8 +24,8 @@ int rand_bytes(byte *out, unsigned int size) {
         return CRP_ERR;
     }
 
-    unsigned int seed;
-    if(!fread(&seed, sizeof(unsigned int), 1, urand)) {
+    u32 seed;
+    if(!fread(&seed, sizeof(u32), 1, urand)) {
         printf("couldn't read seed from '/dev/urandom/'.\n");
         fclose(urand);
         return CRP_ERR;
@@ -31,13 +33,13 @@ int rand_bytes(byte *out, unsigned int size) {
     srand(seed);
 
     while (size--) {
-        byte combine;
+        u8 combine;
         if(!fread(&combine, 1, 1, urand)) {
             printf("couldn't read byte from '/dev/urandom/'.\n");
             fclose(urand);
             return CRP_ERR;
         }
-        byte res = (rand() * combine) + 0x1485914;
+        u8 res = (rand() * combine) + 0x1485914;
         res ^= ((rand() * 0x7fbfb + 2) / 3 + seed >> 2);
 
         out[size] = res;
@@ -50,14 +52,14 @@ int rand_bytes(byte *out, unsigned int size) {
 
 // to decrypt swap ciphertext with plaintext
 // keylen: messagelen, ciphertextlen: messagelen
-int ciph_otp(byte *plaintext, unsigned int pt_len, byte *key, byte **ciphertext, unsigned int *ct_len) {
+i32 ciph_otp(u8 *plaintext, u32 pt_len, u8 *key, u8 **ciphertext, u32 *ct_len) {
     if (!*ciphertext) {
         *ciphertext = malloc(pt_len);
         if (!*ciphertext)
             return CRP_ERR;
     }
 
-    unsigned int i;
+    u32 i;
     for (i = 0; i < pt_len; ++i)
         (*ciphertext)[i] = plaintext[i] ^ key[i];
     *ct_len = i;
@@ -67,30 +69,30 @@ int ciph_otp(byte *plaintext, unsigned int pt_len, byte *key, byte **ciphertext,
 
 // to decrypt swap ciphertext with plaintext
 // keylen: <1, 256>, cipheretxtlen: messagelen
-int ciph_rc4(byte *plaintext, unsigned int pt_len, byte *key, unsigned int key_len, byte **ciphertext, unsigned int *ct_len) {
+i32 ciph_rc4(u8 *plaintext, u32 pt_len, u8 *key, u32 key_len, u8 **ciphertext, u32 *ct_len) {
     if (!*ciphertext) {
         *ciphertext = malloc(pt_len);
         if (!*ciphertext)
             return CRP_ERR;
     }
     
-    byte s[256];
-    byte i = 0, j = 0;
+    u8 s[256];
+    u8 i = 0, j = 0;
     for (i = 0; i < 255; ++i)
         s[i] = i;
     for (i = 0; i < 255; ++i) {
         j = (j + s[i] + key[i % key_len]) % 256;
-        byte temp = s[i];
+        u8 temp = s[i];
         s[i] = s[j];
         s[j] = temp;
     }
 
-    unsigned int k = i = j = 0;
+    u32 k = i = j = 0;
     for (k = 0; k < pt_len; ++k) {
         i = (i + 1) % 256;
         j = (j + s[i]) % 256;
 
-        byte temp = s[i];
+        u8 temp = s[i];
         s[i] = s[j];
         s[j] = temp;
 
@@ -101,13 +103,13 @@ int ciph_rc4(byte *plaintext, unsigned int pt_len, byte *key, unsigned int key_l
     return CRP_OK;
 }
 
-int main() {
-    byte pt[] = "zupa.";
-    byte *key = malloc(32);
-    byte *ct = NULL;
+i32 main() {
+    u8 pt[] = "zupa.";
+    u8 *key = malloc(32);
+    u8 *ct = NULL;
 
     rand_bytes(key, 32);
-    unsigned int ct_len;
+    u32 ct_len;
     ciph_rc4(pt, sizeof(pt), key, 32, &ct, &ct_len);
 
     printf("plaintext: %s\n", pt);
@@ -115,8 +117,8 @@ int main() {
     printf("ciphertext (hex): ");
     hexdump(ct, ct_len);
 
-    byte *dec_ct = NULL;
-    unsigned int dec_ct_len;
+    u8 *dec_ct = NULL;
+    u32 dec_ct_len;
     ciph_rc4(ct, ct_len, key, 32, &dec_ct, &dec_ct_len);
 
     printf("decrypted ciphertext: %s\n", dec_ct);
