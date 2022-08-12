cfd87f800c0e6967940dda086c2c930b0ceaaf6a
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-08-12 16:40:06 +0000

    implement sha224

--- main.c
+++ main.c
@@ -210,14 +210,14 @@ i32 hash_sha1(u8 *plaintext, u32 pt_len, u8 **digest) {
     return CRP_OK;
 }
 
-// digestlen: 32
-i32 hash_sha256(u8 *plaintext, u32 pt_len, u8 **digest) {
+// digestlen: 28
+i32 hash_sha224(u8 *plaintext, u32 pt_len, u8 **digest) {
     if (!*digest) {
         *digest = malloc(32);
         if (!*digest)
             return CRP_ERR;
     }
-    u32 hash[8] = {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19};
+    u32 hash[8] = {0xc1059ed8, 0x367cd507, 0x3070dd17, 0xf70e5939, 0xffc00b31, 0x68581511, 0x64f98fa7, 0xbefa4fa4};
     u32 k[64] = {
         0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
         0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -226,7 +226,7 @@ i32 hash_sha256(u8 *plaintext, u32 pt_len, u8 **digest) {
         0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
         0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
         0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2 
+        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
     };
     u32 padded_len = 64 * ((pt_len + 64 - 1) / 64);
     if (pt_len % 64 == 0 || pt_len % 64 >= 56) padded_len += 64;
@@ -244,7 +244,86 @@ i32 hash_sha256(u8 *plaintext, u32 pt_len, u8 **digest) {
             words[j] = SWAPENDIAN32(*(unsigned int*)(pad_plaintext + i + j * 4));
 
         for (u32 j = 16; j < 64; j++)
-            words[j] = words[j - 16] + (RIGHTROTATE32(words[j - 15], 7) ^ RIGHTROTATE32(words[j - 15], 18) ^ (words[j - 15] >> 3)) + words[j - 7] 
+            words[j] = words[j - 16] + (RIGHTROTATE32(words[j - 15], 7) ^ RIGHTROTATE32(words[j - 15], 18) ^ (words[j - 15] >> 3)) + words[j - 7]
+                + (RIGHTROTATE32(words[j - 2], 17) ^ RIGHTROTATE32(words[j - 2], 19) ^ (words[j - 2] >> 10));
+
+        u32 a = hash[0];
+        u32 b = hash[1];
+        u32 c = hash[2];
+        u32 d = hash[3];
+        u32 e = hash[4];
+        u32 f = hash[5];
+        u32 g = hash[6];
+        u32 h = hash[7];
+
+        for (u32 j = 0; j < 64; ++j) {
+            u32 s1 = RIGHTROTATE32(e, 6) ^ RIGHTROTATE32(e, 11) ^ RIGHTROTATE32(e, 25);
+            u32 ch = (e & f) ^ (~e & g);
+            u32 t1 = h + s1 + ch + k[j] + words[j];
+            u32 s0 = RIGHTROTATE32(a, 2) ^ RIGHTROTATE32(a, 13) ^ RIGHTROTATE32(a, 22);
+            u32 maj = (a & b) ^ (a & c) ^ (b & c);
+            u32 t2 = s0 + maj;
+
+            h = g;
+            g = f;
+            f = e;
+            e = d + t1;
+            d = c;
+            c = b;
+            b = a;
+            a = t1 + t2;
+        }
+        hash[0] += a;
+        hash[1] += b;
+        hash[2] += c;
+        hash[3] += d;
+        hash[4] += e;
+        hash[5] += f;
+        hash[6] += g;
+        hash[7] += h;
+    }
+    for (u32 i = 0; i < 8; ++i)
+        hash[i] = SWAPENDIAN32(hash[i]);
+    memcpy(*digest, hash, 28);
+    free(pad_plaintext);
+    return CRP_OK;
+}
+
+// digestlen: 32
+i32 hash_sha256(u8 *plaintext, u32 pt_len, u8 **digest) {
+    if (!*digest) {
+        *digest = malloc(32);
+        if (!*digest)
+            return CRP_ERR;
+    }
+    u32 hash[8] = {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19};
+    u32 k[64] = {
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+    };
+    u32 padded_len = 64 * ((pt_len + 64 - 1) / 64);
+    if (pt_len % 64 == 0 || pt_len % 64 >= 56) padded_len += 64;
+    u8 *pad_plaintext = malloc(padded_len);
+    memcpy(pad_plaintext, plaintext, pt_len);
+    pad_plaintext[pt_len] = 0x80;
+    memset(pad_plaintext + pt_len + 1, 0, padded_len - pt_len - 9);
+    u64 footer = pt_len * 8;
+    footer = SWAPENDIAN32((footer & 0xFFFFFFFF00000000) >> 32) | (SWAPENDIAN32((footer & 0xFFFFFFFF)) << 32);
+    memcpy(pad_plaintext + padded_len - 8, &footer, 8);
+    for (u32 i = 0; i < padded_len; i += 64) {
+        u32 words[64];
+
+        for (u32 j = 0; j < 16; ++j)
+            words[j] = SWAPENDIAN32(*(unsigned int*)(pad_plaintext + i + j * 4));
+
+        for (u32 j = 16; j < 64; j++)
+            words[j] = words[j - 16] + (RIGHTROTATE32(words[j - 15], 7) ^ RIGHTROTATE32(words[j - 15], 18) ^ (words[j - 15] >> 3)) + words[j - 7]
                 + (RIGHTROTATE32(words[j - 2], 17) ^ RIGHTROTATE32(words[j - 2], 19) ^ (words[j - 2] >> 10));
 
         u32 a = hash[0];
@@ -316,7 +395,7 @@ i32 ciph_rc4(u8 *plaintext, u32 pt_len, u8 *key, u32 key_len, u8 **ciphertext, u
         if (!*ciphertext)
             return CRP_ERR;
     }
-    
+
     u8 s[256];
     u8 i = 0, j = 0;
     for (i = 0; i < 255; ++i)
