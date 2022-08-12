4af27fb9ddf44b2c82f7c09bf8631b6bb1ba3959
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-08-12 16:40:50 +0000

    implement sha512

--- main.c
+++ main.c
@@ -16,6 +16,11 @@ typedef int32_t i32;
 #define RIGHTROTATE32(n, d) ( ( (n) >> (d) ) | ( (n) << (32 - (d)) ) )
 #define SWAPENDIAN32(n) ( ( ( (n) & 0xff ) << 24 ) | ( ( (n) & 0xff00 ) << 8 ) | ( ( (n) & 0xff0000 ) >> 8 ) | ( ( (n) & 0xff000000 ) >> 24 ) )
 
+#define LEFTROTATE64(n, d) ( ( (n) << (d) ) | ( (n) >> (64 - (d)) ) )
+#define RIGHTROTATE64(n, d) ( ( (n) >> (d) ) | ( (n) << (64 - (d)) ) )
+#define SWAPENDIAN64(n) ( ( ( (n) & 0xff ) << 56 ) | ( ( (n) & 0xff00 ) << 40 ) | ( ( (n) & 0xff0000 ) << 24 ) | ( ( (n) & 0xff000000 ) << 8 ) \
+        | ( ( (n) & 0xff00000000 ) >> 8 ) | ( ( (n) & 0xff0000000000 ) >> 24 ) | ( ( (n) & 0xff000000000000 ) >> 40) | ( ( (n) & 0xff00000000000000 ) >> 56 ) )
+
 void hexdump(u8 *in, u32 len) {
     for (u32 i = 0; i < len; ++i)
         printf("%.2hhx", in[i]);
@@ -368,6 +373,95 @@ i32 hash_sha256(u8 *plaintext, u32 pt_len, u8 **digest) {
     return CRP_OK;
 }
 
+// digestlen: 64
+i32 hash_sha512(u8 *plaintext, u32 pt_len, u8 **digest) {
+    if (!*digest) {
+        *digest = malloc(64);
+        if (!*digest)
+            return CRP_ERR;
+    }
+    u64 hash[8] = {0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1, 0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179};
+    u64 k[80] = {
+        0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc, 0x3956c25bf348b538,
+        0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118, 0xd807aa98a3030242, 0x12835b0145706fbe,
+        0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2, 0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235,
+        0xc19bf174cf692694, 0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65,
+        0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5, 0x983e5152ee66dfab,
+        0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4, 0xc6e00bf33da88fc2, 0xd5a79147930aa725,
+        0x06ca6351e003826f, 0x142929670a0e6e70, 0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed,
+        0x53380d139d95b3df, 0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b,
+        0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30, 0xd192e819d6ef5218,
+        0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8, 0x19a4c116b8d2d0c8, 0x1e376c085141ab53,
+        0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8, 0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373,
+        0x682e6ff3d6b2b8a3, 0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec,
+        0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b, 0xca273eceea26619c,
+        0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178, 0x06f067aa72176fba, 0x0a637dc5a2c898a6,
+        0x113f9804bef90dae, 0x1b710b35131c471b, 0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc,
+        0x431d67c49c100d4c, 0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817
+    };
+    u32 padded_len = 128 * ((pt_len + 128 - 1) / 128);
+    if (pt_len % 128 == 0 || pt_len % 128 >= 112) padded_len += 128;
+    u8 *pad_plaintext = malloc(padded_len);
+    memcpy(pad_plaintext, plaintext, pt_len);
+    pad_plaintext[pt_len] = 0x80;
+    memset(pad_plaintext + pt_len + 1, 0, padded_len - pt_len - 17);
+    u64 footer = pt_len * 8;
+    footer = SWAPENDIAN32((footer & 0xFFFFFFFF00000000) >> 32) | (SWAPENDIAN32((footer & 0xFFFFFFFF)) << 32);
+    memset(pad_plaintext + padded_len - 16, 0, 8);
+    memcpy(pad_plaintext + padded_len - 8, &footer, 8);
+    for (u32 i = 0; i < padded_len; i += 128) {
+        u64 words[80];
+
+        for (u32 j = 0; j < 16; ++j)
+            words[j] = SWAPENDIAN64(*(u64*)(pad_plaintext + i + j * 8));
+
+        for (u32 j = 16; j < 80; j++)
+            words[j] = words[j - 16] + (RIGHTROTATE64(words[j - 15], 1) ^ RIGHTROTATE64(words[j - 15], 8) ^ (words[j - 15] >> 7)) + words[j - 7]
+                + (RIGHTROTATE64(words[j - 2], 19) ^ RIGHTROTATE64(words[j - 2], 61) ^ (words[j - 2] >> 6));
+
+        u64 a = hash[0];
+        u64 b = hash[1];
+        u64 c = hash[2];
+        u64 d = hash[3];
+        u64 e = hash[4];
+        u64 f = hash[5];
+        u64 g = hash[6];
+        u64 h = hash[7];
+
+        for (u32 j = 0; j < 80; ++j) {
+            u64 s1 = RIGHTROTATE64(e, 14) ^ RIGHTROTATE64(e, 18) ^ RIGHTROTATE64(e, 41);
+            u64 ch = (e & f) ^ (~e & g);
+            u64 t1 = h + s1 + ch + k[j] + words[j];
+            u64 s0 = RIGHTROTATE64(a, 28) ^ RIGHTROTATE64(a, 34) ^ RIGHTROTATE64(a, 39);
+            u64 maj = (a & b) ^ (a & c) ^ (b & c);
+            u64 t2 = s0 + maj;
+
+            h = g;
+            g = f;
+            f = e;
+            e = d + t1;
+            d = c;
+            c = b;
+            b = a;
+            a = t1 + t2;
+        }
+        hash[0] += a;
+        hash[1] += b;
+        hash[2] += c;
+        hash[3] += d;
+        hash[4] += e;
+        hash[5] += f;
+        hash[6] += g;
+        hash[7] += h;
+    }
+    for (u32 i = 0; i < 8; ++i)
+        hash[i] = SWAPENDIAN64(hash[i]);
+    memcpy(*digest, hash, 64);
+    free(pad_plaintext);
+    return CRP_OK;
+}
+
+
 // if *ciphertext is NULL, the cipher function mallocs the needed memory which is handed to the user
 
 // to decrypt swap ciphertext with plaintext
@@ -426,8 +520,8 @@ i32 ciph_rc4(u8 *plaintext, u32 pt_len, u8 *key, u32 key_len, u8 **ciphertext, u
 i32 main() {
     u8 pt[] = "zupa.";
     u8 *digest = NULL;
-    hash_sha256(pt, strlen(pt), &digest);
+    hash_sha512(pt, strlen(pt), &digest);
     printf("digest: ");
-    hexdump(digest, 32);
+    hexdump(digest, 64);
     free(digest);
 }
